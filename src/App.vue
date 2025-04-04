17f8c2f502737a65d3058c17f6f7f25a8536b8aa
reffcp <cristian.covarrubias@konfio.mx>
2025-04-03 18:17:03 -0600

    feat: :sparkles: se consume api de rick and morty desde axios y se usa un componente hijo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,59 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import ExternalComponent from './components/ExternalComponent.vue'
+const datos = ref([])
+let pagina = Math.floor(Math.random() * 34 + 1)
+
+onMounted(async () => {
+  await getDatos()
+
+  setTimeout(async () => {
+    pagina = Math.floor(Math.random() * 34 + 1)
+    await getDatos()
+  }, 3000)
+})
+
+function hacerAlgo() {
+  console.log('se hizo click.')
+}
+
+async function getDatos() {
+  try {
+    const datosTransformados = []
+    const respuesta = await axios.get('https://rickandmortyapi.com/api/character/?page=' + pagina)
+    //console.log(respuesta.data.results)
+    respuesta.data.results.forEach((element) => {
+      console.log(element)
+      let elementoTemporal = {
+        titulo: element.name,
+        contenido: element.status,
+        imagen: element.image,
+      }
+      datosTransformados.push(elementoTemporal)
+    })
+    console.log(datosTransformados)
+    datos.value = datosTransformados
+    console.log(datos)
+  } catch (error) {
+    console.log('No se pudo acceder a la librería remota', error.message)
+  }
+}
+</script>
 
 <template>
-  <p>Hola mundo</p>
+  <div class="container-expand">
+    <div class="row">
+      <div v-for="dato in datos" class="col-2">
+        <ExternalComponent :image="dato.imagen" :contenido="dato.contenido" :titulo="dato.titulo" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+.parrafo {
+  color: red;
+  font-size: 54px;
+}
+</style>
---
+++ src/components/ExternalComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  titulo: String,
+  contenido: String,
+  image: String,
+})
+</script>
+
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="image" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{{ titulo }}</h5>
+      <p class="card-text">
+        {{ contenido }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
